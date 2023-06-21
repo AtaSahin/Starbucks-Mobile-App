fba68cc34ca49c6a6790c1236710ac7c0c721fa0
Ata Sahin <ata@letsparky.com>
2023-06-22 01:59:22 +0300

    formatted code

--- src/navigation/pages/auth/LoginPage/LoginPage.tsx
+++ src/navigation/pages/auth/LoginPage/LoginPage.tsx
@@ -9,16 +9,17 @@ const LoginScreen = () => {
   const navigation =useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [value, setValue] = useState("");
+  const [formattedValue, setFormattedValue] = useState("");
+  const phoneInput = useRef<PhoneInput>(null);
+  
   const handleLoginPress = () => {
     console.log('Phone Number:', value);
     navigation.navigate("RootNavigation")
     
 
   };
-  const [value, setValue] = useState("");
-  const [formattedValue, setFormattedValue] = useState("");
-  const phoneInput = useRef<PhoneInput>(null);
+  
   return (
     <View style={styles.container}>
 <LoginBG></LoginBG>
