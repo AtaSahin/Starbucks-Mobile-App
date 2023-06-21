c43aca09504767d8aecd8176b8031a4a8a4ab466
Ata Sahin <ata@letsparky.com>
2023-06-21 19:22:08 +0300

    quantity hook added

--- src/components/coffeItems/CoffeItems.tsx
+++ src/components/coffeItems/CoffeItems.tsx
@@ -1,35 +1,59 @@
-import React from 'react';
-import { View,ScrollView,Text,Pressable, TouchableOpacity } from 'react-native';
+import React,{useState} from 'react';
+import { View,Text, TouchableOpacity } from 'react-native';
 import HazelnutImage from '../../navigation/assets/svgImages/CoffeImages/HazelnutCoffe';
 import styles from './CoffeItems.styles';
 
 function CoffeItems() {
-
+const [quantity, setQuantity]= useState(0)
+const [showQuantity, setShowQuantity]=useState(false)
 
     return (
 
-<   View style={styles.coffeContainer}>
+    <View style={styles.coffeContainer}>
 
     <View style={styles.coffeItemTextContainer}>
     <Text style={styles.coffeItemText}> Hazelnut Coffee  </Text>
     <Text style={styles.coffePriceText}>$5</Text>
     </View>
    
+   <View style={{left:5}}>
     <HazelnutImage/>
+    </View>
 
     <View>
     <TouchableOpacity>
 
     <View  style={styles.addButtonContainer}>
 
-    <TouchableOpacity style={styles.addButton}>
+    <TouchableOpacity
+    onPress={()=> {
+    setQuantity(quantity+1)
+    setShowQuantity(true)
+
+    }}
+    style={styles.addButton}
+    >
     <Text style={styles.AddButtonStyles}>Add</Text>
     </TouchableOpacity>
      
+    {showQuantity && 
+    
+    <View style={styles.quantityContainer}>
+    <Text style={styles.quantityText}>{quantity}</Text>
+    <View style={styles.plusMinus}>
+    <Text style={{fontSize:20,bottom:6.5,left:8}}onPress={()=> setQuantity(quantity-1)}>__</Text>
+    <View style={{margin:30}}/>
+   <Text  style={{fontSize:22,right:8}}onPress={()=> setQuantity(quantity+1)}>+</Text>
+    </View>
 
+
+    </View>
+    
+    }
  
     </View>
     </TouchableOpacity>
+  
     </View>
   </View>
     );
--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,64 +1,61 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import HomeScreen from './pages/HomeScreen/HomeScreen';
-import CreateOrderPage from './pages/CreateOrder/createOrderPage';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './pages/auth/LoginPage/LoginPage';
-import SplashPage from './pages/SplashPage/SplashPage';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome";
+import HomeScreen from "./pages/HomeScreen/HomeScreen";
+import CreateOrderPage from "./pages/CreateOrder/createOrderPage";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./pages/auth/LoginPage/LoginPage";
+import SplashPage from "./pages/SplashPage/SplashPage";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const RootNavigation = () => {
   return (
-
-   <Tab.Navigator initialRouteName='HomeScreen'
-   
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Starbucks') {
-            iconName = 'home';
-            color = focused ? '#4AA366' : '#6F8094';
-          }
-
-          if (route.name === 'Create Order') {
-            iconName = 'coffee';
-            color = focused ? '#4AA366' : '#6F8094';
-          }
-
-          return <Icon name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        showLabel: false, // Hide the screen names in the tab bar
-      }}
-    >
-      <Tab.Screen name="Starbucks" 
-      component={HomeScreen} />
-      <Tab.Screen name="Create Order"
-       component={CreateOrderPage} />
-      
-
-
+    <Tab.Navigator initialRouteName="HomeScreen">
+      <Tab.Screen
+        name="Starbucks"
+        component={HomeScreen}
+        options={{
+          headerShown: true,
+          headerStyle: { elevation: 5, shadowOpacity: 5, borderBottomWidth: 1 },
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="coffee" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Create Order"
+        component={CreateOrderPage}
+        options={{
+          headerShown: true,
+          headerStyle: { elevation: 5, shadowOpacity: 5, borderBottomWidth: 1 },
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
-
-
   );
 };
 
 const StackNavigations = () => {
-  
   return (
-    <NavigationContainer >
-      <Stack.Navigator initialRouteName="SplashPage" 
-     screenOptions={{headerShown:false}}>
-      <Stack.Screen name="RootNavigation" component={RootNavigation} />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SplashPage"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="RootNavigation" component={RootNavigation} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="Starbucks" component={HomeScreen} />
+        <Stack.Screen
+          name="Starbucks"
+          component={HomeScreen}
+          options={{
+            headerShown: true,
+          }}
+        />
         <Stack.Screen name="CreateOrderPage" component={CreateOrderPage} />
         <Stack.Screen name="SplashPage" component={SplashPage} />
       </Stack.Navigator>
