f1a12ba70d6662e401e01462447de4656594c9b6
Ata Sahin <atasahin05@gmail.com>
2023-07-07 00:49:47 +0300

    login UI adjustment

--- src/navigation/pages/auth/LoginPage/LoginPage.styles.ts
+++ src/navigation/pages/auth/LoginPage/LoginPage.styles.ts
@@ -1,6 +1,6 @@
-import { StyleSheet, Dimensions } from "react-native";
-import { scale } from "react-native-size-matters";
-const { width, height } = Dimensions.get("window");
+import {StyleSheet, Dimensions} from 'react-native';
+import {scale} from 'react-native-size-matters';
+const {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -10,21 +10,23 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     bottom: height * 0.25,
     right: width * 0.02,
   },
   input: {
-    width: "100%",
-    height: 40,
+    width: 320,
+    height: 50,
     borderWidth: 1,
-    borderColor: "gray",
+    borderColor: 'gray',
+    backgroundColor: 'white',
     borderRadius: 5,
+    bottom: 100,
     marginBottom: 10,
     paddingHorizontal: 10,
   },
   loginButton: {
-    backgroundColor: "#B5F0C8",
+    backgroundColor: '#B5F0C8',
     paddingVertical: 10,
     paddingHorizontal: 20,
     width: width * 0.8,
@@ -33,9 +35,9 @@ const styles = StyleSheet.create({
     height: height * 0.09,
   },
   loginButtonText: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
     fontSize: scale(15),
     top: height * 0.015,
   },
@@ -47,19 +49,19 @@ const styles = StyleSheet.create({
   },
   pageComponents: {
     bottom: height * 0.65,
-    alignContent: "center",
-    alignSelf: "center",
+    alignContent: 'center',
+    alignSelf: 'center',
     right: scale(15),
   },
-  activatedButton:{
-    backgroundColor: "#4AA366",
+  activatedButton: {
+    backgroundColor: '#4AA366',
     paddingVertical: 10,
     paddingHorizontal: 20,
     width: width * 0.8,
     top: height * 0.05,
     borderRadius: scale(10),
     height: height * 0.09,
-  }
+  },
 });
 
 export default styles;
--- src/navigation/pages/auth/LoginPage/LoginPage.tsx
+++ src/navigation/pages/auth/LoginPage/LoginPage.tsx
@@ -1,59 +1,61 @@
-import React, { useState,useRef } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet,Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Button,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import styles from './LoginPage.styles';
-import PhoneInput from "react-native-phone-number-input";
+import PhoneInput from 'react-native-phone-number-input';
 import LoginBG from '../../../assets/svgImages/loginBg';
 
 const LoginScreen = () => {
-  const navigation =useNavigation();
+  const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [value, setValue] = useState(0);
-  const [formattedValue, setFormattedValue] = useState("");
+  const [formattedValue, setFormattedValue] = useState('');
   const phoneInput = useRef<PhoneInput>(null);
-  
+
   const handleLoginPress = () => {
     console.log('Phone Number:', value);
-    navigation.navigate("RootNavigation")
-    
-
+    navigation.navigate('RootNavigation');
   };
-  
+
   return (
     <View style={styles.container}>
-<LoginBG></LoginBG>
-<View style={styles.pageComponents}>
-      <Text style={styles.title}> Welcome To Starbucks</Text>
+      <LoginBG></LoginBG>
+      <View style={styles.pageComponents}>
+        <Text style={styles.title}> Welcome To Starbucks</Text>
 
-      <PhoneInput
-            ref={phoneInput}
-            defaultValue={value}
-            defaultCode="TR"
-            layout="first"
-            onChangeText={(text) => {
-              setValue(text);
-            }}
-            onChangeFormattedText={(text) => {
-              setFormattedValue(text);
-            }}
-            withShadow
-            autoFocus
-            containerStyle={styles.phonInputStyle}
-          />
-          <TouchableOpacity
-            style={value < 10 ? styles.loginButton : styles.activatedButton}
-           
-            onPress={handleLoginPress}
-          >
-            <Text style={styles.loginButtonText}>Login</Text>
-          </TouchableOpacity>
-
-          </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={text => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={text => setPassword(text)}
+          value={password}
+        />
+        <TouchableOpacity
+          style={
+            email.length > 5 && password.length > 5
+              ? styles.activatedButton
+              : styles.loginButton
+          }
+          onPress={handleLoginPress}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
-
-
 
 export default LoginScreen;
